ac6ef27dcc453ae39b1ffe87ac60ed9d0bb07de9
nju33 <nju33.ki@gmail.com>
2018-06-27 02:52:56 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,40 @@ fn main() {
 
     let scrapbox_img = image::open("scrapbox.png").unwrap();
     let resized_scrapbox_img = scrapbox_img.resize(
-        ((geek_img_width as f32) * 0.25) as u32,
-        ((geek_img_width as f32) * 0.25) as u32,
+        ((geek_img_width as f32) * 0.4) as u32,
+        ((geek_img_width as f32) * 0.4) as u32,
         image::FilterType::Nearest
     );
-    let (resized_scrapbox_img_width, resized_scrapbox_img_height) = geek_img.dimensions();
-
-    // println!("{:?}", resized_scrapbox_img.dimensions());
+    let (resized_scrapbox_img_width, resized_scrapbox_img_height) = resized_scrapbox_img.dimensions();
 
 
     let mut img_buf: image::RgbaImage = ImageBuffer::new(geek_img_width, geek_img_height);
     let mut img_buf2: image::RgbaImage = ImageBuffer::new(geek_img_width, geek_img_height);
 
 
-    // println!("{:?}", img.color());
     for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
-        let relative_x = ((geek_img_width as f32) * 0.75) as u32;
-        let relative_y = ((geek_img_height as f32) * 0.75) as u32;
+        let relative_x = ((geek_img_width as f32) * 0.6) as u32;
+        let relative_y = ((geek_img_height as f32) * 0.6) as u32;
 
         let mut real_x = x;
         let mut real_y = y;
         let mut target = &geek_img;
 
-        if x + resized_scrapbox_img_width > geek_img_width {
-            real_x = geek_img_width - x;
-            target = &resized_scrapbox_img;
+        if x + resized_scrapbox_img_width > geek_img_width && y + resized_scrapbox_img_height > geek_img_height {
+            let maybe_real_x = x + resized_scrapbox_img_width - geek_img_width;
+            let maybe_real_y = y + resized_scrapbox_img_height - geek_img_height;
+
+            let maybe_real_pixel = resized_scrapbox_img.get_pixel(maybe_real_x, maybe_real_y);
+            let alpha_value = maybe_real_pixel.channels()[3];
+
+            if (alpha_value != 0) {
+                real_x = maybe_real_x;
+                real_y = maybe_real_y;
+                target = &resized_scrapbox_img;
+            }
         }
 
-        if y + resized_scrapbox_img_height > geek_img_height {
-            real_y = geek_img_height - y;
-            target = &resized_scrapbox_img;
-        }
-
-        println!("real_x={}, real_y={}", real_x, real_y);
-        img_buf2.put_pixel(x, y, target.get_pixel(real_x, real_y));
+        img_buf2.put_pixel(x, y, target.get_pixel(real_x, real_y).to_rgba());
     }
 
     img_buf2.save("icon.png");
