95ac46e347b2a95f352f33fbbd497e436848e7af
nju33 <nju33.ki@gmail.com>
2018-10-10 15:45:40 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate image;
 
-use image::{GenericImage, ImageBuffer, Pixel};
+use image::{GenericImage, GenericImageView, ImageBuffer, Pixel};
 
 fn main() {
     let mut geek_img = image::open("geek.png").unwrap();
@@ -10,31 +10,32 @@ fn main() {
     let resized_scrapbox_img = scrapbox_img.resize(
         ((geek_img_width as f32) * 0.4) as u32,
         ((geek_img_width as f32) * 0.4) as u32,
-        image::FilterType::Nearest
+        image::FilterType::Nearest,
     );
-    let (resized_scrapbox_img_width, resized_scrapbox_img_height) = resized_scrapbox_img.dimensions();
-
+    let (resized_scrapbox_img_width, resized_scrapbox_img_height) =
+        resized_scrapbox_img.dimensions();
 
     let mut img_buf: image::RgbaImage = ImageBuffer::new(geek_img_width, geek_img_height);
     let mut img_buf2: image::RgbaImage = ImageBuffer::new(geek_img_width, geek_img_height);
 
-
-    for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
-        let relative_x = ((geek_img_width as f32) * 0.6) as u32;
-        let relative_y = ((geek_img_height as f32) * 0.6) as u32;
+    for (x, y, _pixel) in img_buf.enumerate_pixels_mut() {
+        let _relative_x = ((geek_img_width as f32) * 0.6) as u32;
+        let _relative_y = ((geek_img_height as f32) * 0.6) as u32;
 
         let mut real_x = x;
         let mut real_y = y;
         let mut target = &geek_img;
 
-        if x + resized_scrapbox_img_width > geek_img_width && y + resized_scrapbox_img_height > geek_img_height {
+        if x + resized_scrapbox_img_width > geek_img_width
+            && y + resized_scrapbox_img_height > geek_img_height
+        {
             let maybe_real_x = x + resized_scrapbox_img_width - geek_img_width;
             let maybe_real_y = y + resized_scrapbox_img_height - geek_img_height;
 
             let maybe_real_pixel = resized_scrapbox_img.get_pixel(maybe_real_x, maybe_real_y);
             let alpha_value = maybe_real_pixel.channels()[3];
 
-            if (alpha_value != 0) {
+            if alpha_value != 0 {
                 real_x = maybe_real_x;
                 real_y = maybe_real_y;
                 target = &resized_scrapbox_img;
